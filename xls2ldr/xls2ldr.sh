3b4a02e8b62fd46b2f6297aa85e16928d97aa45e
kawasima <kawasima1016@gmail.com>
2009-12-17 15:39:05 +0900

    for multiple sheets

--- xls2ldr/xls2ldr.sh
+++ xls2ldr/xls2ldr.sh
@@ -3,27 +3,27 @@
 shopt -s extglob
 
 function field_list() {
-	local i=0
-	for f in $@ ; do
-		printf "$f"
-		if [[ $f == *_TM ]] ; then
-			printf " \"TO_DATE(:%s, 'YYYY/MM/DD HH24:MI:SS')\" " $f
-		elif [[ $f == *_DATE ]] ; then
-			printf " \"TO_DATE(:%s, 'YYYY/MM/DD')\" " $f
-		fi
-		if [ $i -lt $(($# - 1)) ] ; then
-			printf ","
-		fi
-		echo ""
-		i=$(($i + 1))
-	done
+    local i=0
+    for f in $@ ; do
+        printf "$f"
+        if [[ $f == *_TM ]] ; then
+            printf " \"TO_DATE(:%s, 'YYYY/MM/DD HH24:MI:SS')\" " $f
+        elif [[ $f == *_DATE ]] ; then
+            printf " \"TO_DATE(:%s, 'YYYY/MM/DD')\" " $f
+        fi
+        if [ $i -lt $(($# - 1)) ] ; then
+            printf ","
+        fi
+        echo ""
+        i=$(($i + 1))
+    done
 }
 
 function create_ctl_file() {
-	local table=$TABLE_NAME
-	local fields=$(< $HEADER_FILE)
+    local table=$TABLE_NAME
+    local fields=$(< $HEADER_FILE)
 
-	cat <<EOF > $CTL_FILE
+    cat <<EOF > $CTL_FILE
 LOAD DATA
 INFILE '${table}.dat'
 TRUNCATE
@@ -37,32 +37,39 @@ EOF
 }
 
 function create_dat_file() {
+    local sheet_no=$1
     local i=0
-    xlhtml -asc -xp:0 $EXCEL_FILE | while read line; do
+    xlhtml -asc -xp:$sheet_no $EXCEL_FILE | while read line; do
         if [ $i -eq 0 ] ; then
             echo $line> $HEADER_FILE
         elif [ "$line" != "" ] ; then
-            echo $line
+            printf "$line\n"
         fi
         i=$(($i+1))
     done > $DAT_FILE
 }
 
-if [ $# == 0 ] ; then
-	echo "no argument"
-	exit 1
-fi
-
-for f in $@ ; do
-    EXCEL_FILE=$f
-    TABLE_NAME=$(basename $(basename $f .xls) .xlsx)
+function parse_excel() {
+    local sheet_no=$1
+    TABLE_NAME=$2
 
     HEADER_FILE="${TABLE_NAME}.header"
     DAT_FILE="${TABLE_NAME}.dat"
     CTL_FILE="${TABLE_NAME}.ctl"
 
-    create_dat_file
+    create_dat_file $sheet_no
     create_ctl_file
     rm $HEADER_FILE
-done
+}
 
+if [ $# == 0 ] ; then
+    echo "no argument"
+    exit 1
+fi
+
+for f in $@ ; do
+    EXCEL_FILE=$f
+    xlhtml -asc -dp $EXCEL_FILE \
+        | perl -ne 'print "$1 $2\n" if /^Page:(\d+) Name:(\w+)/' \
+        | while read line; do eval parse_excel $line; done
+done
